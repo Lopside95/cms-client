3c09bce4abd4c862b8663b4d545f731e5ab94e3d
James Wallington <jameswallington11@gmail.com>
2025-01-09 11:06:29 +0000

    install tailwind and shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
 }
 
 export default App;
